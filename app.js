68522f7efae43f3c1c6326cf6081a71c5fa9bc8f
darthyoshi007 <doshi.parth9@gmail.com>
2016-02-27 12:10:59 -0800

    Browserify + Firebase
    added dependencies
    
    run npm install

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 
+var Firebase = require("firebase");
 var ref = new Firebase("https://herokuhackathon.firebaseio.com"); //links to firebase server
+// var app = angular.module("herokuhackathon", ["firebase"]);
+// app.controller("somethingggggggggggggg", function($scope, $firebaseObject) {
+//   // download the data into a local object
+//   var syncObject = $firebaseObject(ref);
+//   // synchronize the object with a three-way data binding
+//   syncObject.$bindTo($scope, "data");
+// });
 
-function createUser(){
-  var emailHTML; //string
-  var passwordHTML; //string
+window.createUser = function(){
+  var emailHTML = document.getElementById("email-input").value; //string
+  var passwordHTML = document.getElementById("password-input").value; //string
+  console.log(document.getElementById("email-input").value);
   ref.createUser({
     email    : emailHTML,
     password : passwordHTML
@@ -16,9 +25,9 @@ function createUser(){
   });
 }
 
-function authUser(){
-  var emailHTML; //string
-  var passwordHTML; //string
+window.authUser = function(){
+  var emailHTML = document.getElementById("email-input").value; //string
+  var passwordHTML = document.getElementById("password-input").value; //string
   ref.authWithPassword({
     email    : emailHTML,
     password : passwordHTML
