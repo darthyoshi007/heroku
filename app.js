d45d4663b8097e9faa6229b8238aa164c084f9a8
darthyoshi007 <doshi.parth9@gmail.com>
2016-02-27 17:37:25 -0800

    <insert here>

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ if (localStorage.getItem("uid") != null && window.location.pathname == "./index.
 //   // synchronize the object with a three-way data binding
 //   syncObject.$bindTo($scope, "data");
 // });
+usersRef.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
 window.addProject = function(){
   var projectName = document.getElementById("").value;
@@ -28,7 +33,6 @@ window.addProject = function(){
 window.createUser = function(){
   var emailHTML = document.getElementById("email-input").value; //string
   var passwordHTML = document.getElementById("password-input").value; //string
-  window[emailHTML] = "";
   ref.createUser({
     email    : emailHTML,
     password : passwordHTML
@@ -37,10 +41,7 @@ window.createUser = function(){
       console.log("Error creating user:", error);
     } else {
       var userid = "" + userData.uid;
-      var obj = {};
-      obj[emailHTML] = userid;
-      console.log(userid);
-      ref.push(obj);
+      usersRef.push({emailHTML, userid});
     }
   });
 };
@@ -71,20 +72,28 @@ window.deAuth = function(){
 
 window.teacherOnLoad = function(){
   var courses = ["Math","English","Science"];
-  // Get data from database and put into courses array
-
+  // Get data from Firebase and put into courses array
+  var array = ["one", "two", "three"];
   var courseHTML = [];
   for(var i = 0; i < courses.length; i++){
-    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(\"" + courses[i] + "" + i +"\")'>" + courses[i] + "</span></li>";
+    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(" + courses[i] + "" + i +")'>" + courses[i] + "</span></li>";
   }
-
+  // var courseHTML = $.map(courses, function(course, i) {
+  //   return "<li><a href='" + course + "" + i +"'>" + course + "</a></li>";
+  // });
   document.getElementById("teacherCoursesList").innerHTML = courseHTML.join("");
 };
 
-window.courseTabChange = function(courseID){
-  // Get data from database 
-  var formattedData = "<span>" + courseID + "</span>";
-
-  document.getElementById("projectsForCourse").innerHTML = formattedData;
-
+window.teacherOnLoad = function(){
+  var courses = ["Math","English","Science"];
+  // Get data from Firebase and put into courses array
+  var array = ["one", "two", "three"];
+  var courseHTML = [];
+  for(var i = 0; i < courses.length; i++){
+    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(" + courses[i] + "" + i +")'>" + courses[i] + "</span></li>";
+  }
+  // var courseHTML = $.map(courses, function(course, i) {
+  //   return "<li><a href='" + course + "" + i +"'>" + course + "</a></li>";
+  // });
+  document.getElementById("teacherCoursesList").innerHTML = courseHTML.join("");
 };
--- bundle.js
+++ bundle.js
@@ -19,6 +19,11 @@ if (localStorage.getItem("uid") != null && window.location.pathname == "./index.
 //   // synchronize the object with a three-way data binding
 //   syncObject.$bindTo($scope, "data");
 // });
+usersRef.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
 window.addProject = function(){
   var projectName = document.getElementById("").value;
@@ -29,7 +34,6 @@ window.addProject = function(){
 window.createUser = function(){
   var emailHTML = document.getElementById("email-input").value; //string
   var passwordHTML = document.getElementById("password-input").value; //string
-  window[emailHTML] = "";
   ref.createUser({
     email    : emailHTML,
     password : passwordHTML
@@ -38,10 +42,7 @@ window.createUser = function(){
       console.log("Error creating user:", error);
     } else {
       var userid = "" + userData.uid;
-      var obj = {};
-      obj[emailHTML] = userid;
-      console.log(userid);
-      ref.push(obj);
+      usersRef.push({emailHTML, userid});
     }
   });
 };
@@ -72,23 +73,32 @@ window.deAuth = function(){
 
 window.teacherOnLoad = function(){
   var courses = ["Math","English","Science"];
-  // Get data from database and put into courses array
-
+  // Get data from Firebase and put into courses array
+  var array = ["one", "two", "three"];
   var courseHTML = [];
   for(var i = 0; i < courses.length; i++){
-    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(\"" + courses[i] + "" + i +"\")'>" + courses[i] + "</span></li>";
+    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(" + courses[i] + "" + i +")'>" + courses[i] + "</span></li>";
   }
-
+  // var courseHTML = $.map(courses, function(course, i) {
+  //   return "<li><a href='" + course + "" + i +"'>" + course + "</a></li>";
+  // });
   document.getElementById("teacherCoursesList").innerHTML = courseHTML.join("");
 };
 
-window.courseTabChange = function(courseID){
-  // Get data from database 
-  var formattedData = "<span>" + courseID + "</span>";
-
-  document.getElementById("projectsForCourse").innerHTML = formattedData;
-
+window.teacherOnLoad = function(){
+  var courses = ["Math","English","Science"];
+  // Get data from Firebase and put into courses array
+  var array = ["one", "two", "three"];
+  var courseHTML = [];
+  for(var i = 0; i < courses.length; i++){
+    courseHTML[i] = "<li><span class='menu-item hvr-underline-centerD' onclick='courseTabChange(" + courses[i] + "" + i +")'>" + courses[i] + "</span></li>";
+  }
+  // var courseHTML = $.map(courses, function(course, i) {
+  //   return "<li><a href='" + course + "" + i +"'>" + course + "</a></li>";
+  // });
+  document.getElementById("teacherCoursesList").innerHTML = courseHTML.join("");
 };
+
 },{"firebase":2}],2:[function(require,module,exports){
 /*! @license Firebase v2.4.1
     License: https://www.firebase.com/terms/terms-of-service.html */
